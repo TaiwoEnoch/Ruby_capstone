d32153eb0c0ab4ca1c2d4f6d59b3ba0cc1d626f9
Ritika Rawat <Ritikarawat220@gmail.com>
2023-06-13 13:48:28 +0530

    Add tests

---
+++ spec/genre_spec.rb
@@ -0,0 +1,18 @@
+require_relative '../Classes/genre'
+require_relative '../Classes/item'
+require 'rspec'
+
+describe Genre do
+  test_genre = Genre.new('test')
+  item = Item.new('Title', 'Author', 'Description', '2008/05/05', test_genre)
+
+  it 'test_genre should return "test" as its name' do
+    expect(test_genre.name).to eq('test')
+  end
+
+  it 'After adding one item "item" to test_genre, its items array should include "item"' do
+    test_genre.add_item(item)
+
+    expect(test_genre.items.include?(item)).to be true
+  end
+end
