e602d023e2288731eef79c12098d0f811aa44342
matee-safi <matiullahsafibbc@gmail.com>
2023-06-15 11:16:56 +0430

    remove unnecessary lines to display

--- Modules/list_movie.rb
+++ Modules/list_movie.rb
@@ -3,7 +3,7 @@ class ListMovie
     puts 'Listing all movies...'
     File.foreach('Data/movies.json') do |line|
       movie_data = JSON.parse(line)
-      puts "Title: #{movie_data['title']}, Genre: #{movie_data['genre']}, Publish Date: #{movie_data['publish_date']}, On Spotify: #{movie_data['on_spotify']}, Silent: #{movie_data['silent']}, Source: #{movie_data['source']}"
+      puts "Silent: #{movie_data['silent']}, Source: #{movie_data['source']}"
     end
   end
 end
