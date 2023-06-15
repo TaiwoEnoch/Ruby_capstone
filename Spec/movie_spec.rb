1c0ca993a3edb542d046d017ae4a8289fecf3d2b
matee-safi <matiullahsafibbc@gmail.com>
2023-06-15 16:46:38 +0430

    update tests

--- Spec/movie_spec.rb
+++ Spec/movie_spec.rb
@@ -1,24 +1,16 @@
 require_relative '../Modules/list_movie'
-require 'stringio'
-require 'json'
+require_relative '../Modules/add_movie'
 
-RSpec.describe ListMovie do
-  describe '#list_movies' do
-    it 'prints the details of each movie' do
-      movies_data = [
-        { 'publish_date' => '2022-01-01', 'silent' => true, 'source' => 'Netflix' },
-        { 'publish_date' => '2022-02-01', 'silent' => false, 'source' => 'Amazon Prime' }
-      ]
-
-      movies_file = StringIO.new(movies_data.map { |data| JSON.generate(data) }.join("\n"))
-      allow(File).to receive(:foreach).with('Data/movies.json').and_yield(movies_file)
-
-      expected_output = [
-        "Publish Date: 2022-01-01, Silent: true, Source: Netflix\n",
-        "Publish Date: 2022-02-01, Silent: false, Source: Amazon Prime\n"
-      ]
-
-      expect { subject.list_movies }.to output(expected_output.join).to_stdout_from_any_process
+describe AddMovie do
+    it 'Creates a new Movie' do
+        movie = AddMovie.new
+        expect(movie).to be_a(AddMovie)
     end
-  end
+end
+
+describe ListMovie do
+    it 'Lists all movies' do
+        movie = ListMovie.new
+        expect(movie.list_movies).to be_a(ListMovie)
+    end
 end
--- Spec/source_spec.rb
+++ Spec/source_spec.rb
@@ -1,15 +1,8 @@
-# require 'rspec'
-# require_relative '../Classes/source'
-# require_relative '../Modules/list_source'
+require_relative '../Modules/list_source'
 
-# describe Source do
-#     it 'Creates a new Source' do
-#         source = Source.new('True', '2020-9-11')
-#         expect(source).to be_a(Source)
-#     end
-    
-#     it 'Lists all sources' do
-#         source = ListSource.new
-#         expect(source.list_source).to be_a(Array)
-#     end
-# end
+describe ListSource do
+    it 'Lists all sources' do
+        source = ListSource.new
+        expect(source.list_sources).to be_a(ListSource)
+    end
+end
