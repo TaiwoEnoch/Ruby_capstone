9b190d8ea4ccae2dceabd4db92144112a05c5b7a
matee-safi <matiullahsafibbc@gmail.com>
2023-06-15 11:15:40 +0430

    reduce unnecessary prompts

--- Classes/movie.rb
+++ Classes/movie.rb
@@ -2,15 +2,11 @@ require_relative 'item'
 require_relative 'source'
 
 class Movie < Item
-  attr_accessor :title, :genre, :publish_date, :on_spotify, :silent, :source
+  attr_accessor :silent, :source
 
-  def initialize(title = nil, genre = nil, publish_date = nil, on_spotify = nil, silent = nil, source = nil)
-    @title = title
-    @genre = genre
-    @publish_date = publish_date
-    @on_spotify = on_spotify
+  def initialize(silent, source)
+    super(nil, nil, nil, nil, source)
     @silent = silent
-    @source = source
   end
 
   def can_be_archived?
--- Modules/add_movie.rb
+++ Modules/add_movie.rb
@@ -3,24 +3,12 @@ require_relative '../Classes/movie'
 
 class AddMovie
   def initialize
-    @movie = Movie.new(nil, nil, nil, nil, nil, nil)
+    @movie = Movie.new(nil, nil)
   end
 
   def run
     puts 'Adding a movie...'
     puts 'Please enter the following information:'
-
-    print 'Title: '
-    @movie.title = gets.chomp
-
-    print 'Genre: '
-    @movie.genre = gets.chomp
-
-    print 'Publish Date (Year-MM-DD): '
-    @movie.publish_date = gets.chomp
-
-    print 'On Spotify (Enter t-True/f-False): '
-    @movie.on_spotify = gets.chomp.downcase
 
     print 'Silent (Enter t-True/f-False): '
     @movie.silent = gets.chomp.downcase
@@ -36,10 +24,6 @@ class AddMovie
 
   def save_movie_to_file
     movie_data = {
-      title: @movie.title,
-      genre: @movie.genre,
-      publish_date: @movie.publish_date,
-      on_spotify: @movie.on_spotify,
       silent: @movie.silent,
       source: @movie.source
     }
