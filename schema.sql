60e14e0d7d2983f58ca32706c8470a4256e951f0
TaiwoEnoch <taiwoenoch0@gmail.com>
2023-06-16 09:08:50 +0100

    Modify schema file for creating of table

--- schema.sql
+++ schema.sql
@@ -1,12 +1,25 @@
-CREATE TABLE games (
-  id SERIAL PRIMARY KEY,
-  genre VARCHAR(255),
-  multiplayer BOOLEAN,
-  last_played_at TIMESTAMP
+CREATE DATABASE catalog_for_my_team;
+
+CREATE TABLE label (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  title VARCHAR(100) NOT NULL,
+  color VARCHAR(100) NOT NULL,
+  item_id INT NOT NULL,
+  FOREIGN KEY(item_id) REFERENCES item(id)
 );
 
-CREATE TABLE authors (
-  id SERIAL PRIMARY KEY,
-  first_name VARCHAR(255),
-  last_name VARCHAR(255)
+CREATE TABLE item (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  genre_id INT NOT NULL,
+  author_id INT NOT NULL,
+  label_id INT NOT NULL,
+  published_at DATE NOT NULL,
+  is_archived BOOLEAN NOT NULL,
+);
+
+CREATE TABLE book (
+  item_id INT PRIMARY KEY,
+  publisher VARCHAR(100) NOT NULL,
+  book_cover VARCHAR(100) NOT NULL,
+  FOREIGN KEY (item_id) REFERENCES item (id)
 );
